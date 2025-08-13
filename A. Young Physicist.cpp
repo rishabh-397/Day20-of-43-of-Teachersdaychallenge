e35b53c3dcd77ee4ecf3f2c7aaac09957ccef233
rishabh-397 <rishabh.cha2005@gmail.com>
2025-08-13 23:10:37 +0530

    A. Young Physicist.cpp

---
+++ A. Young Physicist.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int x = 0, y = 0, z = 0;
+    
+    while (n--) {
+        int xi, yi, zi;
+        cin >> xi >> yi >> zi;
+        x += xi;
+        y += yi;
+        z += zi;
+    }
+    
+    if (x == 0 && y == 0 && z == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
